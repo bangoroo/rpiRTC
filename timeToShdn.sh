47e2eddf00c819b56c20111aaadb6d9e8811a692
bangoroo <38109716+bangoroo@users.noreply.github.com>
2019-05-22 19:48:27 +0200

    Update timeToShdn.sh

--- timeToShdn.sh
+++ timeToShdn.sh
@@ -4,11 +4,11 @@ if [[ "$USER" != 'root' ]]; then
 	exit
 fi
 shdn() {
-		echo "Setze Zeit zum Herunterfahren (Minute | Stunde | Tag des Monats | Monat | Tag der Woche)"
-		echo "Bsp.: 0 20 * * *"
-		read time
-		echo "shutdown -h now" >>  /usr/sbin/shdn.sh
-		echo "$time /usr/sbin/shdn.sh" >> /etc/crontab
+    echo "Setze Zeit zum Herunterfahren (Minute | Stunde | Tag des Monats | Monat | Tag der Woche)"
+    echo "Bsp.: 0 20 * * *"
+    read time
+    echo "shutdown -h now" >>  /usr/sbin/shdn.sh
+    echo "$time /usr/sbin/shdn.sh" >> /etc/crontab
 
 			
 }
